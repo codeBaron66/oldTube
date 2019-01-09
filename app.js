6581f1a202487fb372bc529a0e5558d4f813d875
Nicky Croydon <nickc87@hotmail.co.uk>
2019-01-09 16:01:58 +0000

    set up project on c9 development space so can make use of Heroku pushes

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express"),
+      app = express();
+      
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+   res.render("index"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Listening");
+});
